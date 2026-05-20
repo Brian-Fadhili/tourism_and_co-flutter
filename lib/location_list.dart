57fb94fb674652b6cf36e36a002aedc43873af50
Brian Fadhili <brianfadhili05@gmail.com>
2026-05-20 10:32:21 +0300

    Added a progress bar and refresh indicator

--- lib/location_list.dart
+++ lib/location_list.dart
@@ -1,7 +1,9 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'models/location.dart';
 import 'location_detail.dart';
 import 'styles.dart';
+import 'dart:async';
 
 class LocationList extends StatefulWidget {
   @override
@@ -9,13 +11,32 @@ class LocationList extends StatefulWidget {
 }
 
 class _LocationListState extends State<LocationList> {
-
   List<Location> locations = [];
+  bool loading = false;
 
   @override
   void initState() {
     super.initState();
     loadData();
+  }
+
+  Future<void> loadData() async {
+    try {
+      if (!mounted) return;
+      setState(() => loading = true);
+      
+      await Future.delayed(Duration(milliseconds: 3000));
+        final locations = await Location.fetchAll();
+      if (!mounted) return;
+      setState(() {
+        this.locations = locations;
+        loading = false;
+      });
+      
+    } catch (e) {
+      setState(() => loading = false);
+      print("Error loading locations: $e");
+    }
   }
 
   @override
@@ -24,41 +45,61 @@ class _LocationListState extends State<LocationList> {
       appBar: AppBar(
         backgroundColor: Colors.blueGrey,
         title: Text("Locations", style: Styles.titleText),
+        actions: [
+          if(kIsWeb)
+          IconButton(
+            icon: Icon(Icons.refresh),
+            onPressed: loadData,
+          )
+        ]
       ),
-      body: ListView.builder(                                                    
-        itemCount: locations.length,
-        itemBuilder: _itemBuilderListView,
+      body: RefreshIndicator(
+        onRefresh: () => loadData(),
+        child: Column(
+          children: [
+            _renderProgressBar(context),
+            Expanded(child: _renderListView(context)),
+          ],
+        ),
       ),
     );
   }
 
-  loadData() async {
-  try {
-    final locations = await Location.fetchAll();
-    setState(() {
-      this.locations = locations;
-    });
-  } catch (e) {
-    print("Error loading locations: $e");
+  Widget _renderProgressBar(BuildContext context) {
+    return loading
+        ? LinearProgressIndicator(
+            value: null,
+            backgroundColor: Colors.transparent,
+            valueColor: AlwaysStoppedAnimation(Colors.blue.shade200),
+          )
+        : Container();
   }
-}
+
+  Widget _renderListView(BuildContext context) {
+    return ListView.builder(
+      physics: const AlwaysScrollableScrollPhysics(),
+      itemCount: locations.length,
+      itemBuilder: _itemBuilderListView,
+    );
+  }
 
   Widget _itemBuilderListView(BuildContext context, int index) {
     var location = locations[index];
     return ListTile(
-            contentPadding: EdgeInsets.all(10.0),
-            leading: _itemThumbnail(location),
-            title: _itemTile(location),
-            onTap: () {
-              _navigateToLocationDetail(context, location.id);
-            },
-          );
+      contentPadding: EdgeInsets.all(10.0),
+      leading: _itemThumbnail(location),
+      title: _itemTile(location),
+      onTap: () {
+        _navigateToLocationDetail(context, location.id);
+      },
+    );
   }
 
   void _navigateToLocationDetail(BuildContext context, int locationID) {
-    Navigator.push(context, MaterialPageRoute(
-      builder: (context) => LocationDetail(locationID)
-    ));
+    Navigator.push(
+      context,
+      MaterialPageRoute(builder: (context) => LocationDetail(locationID)),
+    );
   }
 
   Widget _itemThumbnail(Location location) {
